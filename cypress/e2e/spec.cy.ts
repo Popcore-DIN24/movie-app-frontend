9da6c327501a7e947bad37133ebc551c96904b9b
Ifeoluwaexam2025 <iogungbe24@students.oamk.fi.com>
2025-10-30 16:04:06 +0200

    hello world deployment

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,10 @@
 describe('basic testing', () => {
   it('passes', () => {
-    cy.visit('/')
+    cy.visit(' http://localhost:5173/')
+  })
+
+  // test window title
+  it('has correct title', ()=>{
+    cy.title().should('contain', 'demo')
   })
 })
