750abdfff812d352500f05e754634fef6a1a5a87
Ifeoluwaexam2025 <iogungbe24@students.oamk.fi.com>
2025-11-06 11:46:52 +0200

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import "./Footerd.css"
+export default function () {
+  return (
+    <footer className="footer">
+    <div>
+       © {new Date().getFullYear()} Popcore_24. All rights reserved.
+    </div></footer>
+  )
+}
+ 
