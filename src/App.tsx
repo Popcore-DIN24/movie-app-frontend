c05d28f8e9cf5c9506b8f9b58ff5a6308e6aaa50
Ifeoluwaexam2025 <iogungbe24@students.oamk.fi.com>
2025-12-04 21:39:25 +0200

    Added ticketing implementation with pdf to qrcode implementation

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import ContactUs from "./pages/user/ContactUs";
 import History from "./pages/user/History";
 import MovieDetails from "./pages/user/MovieDetails";
 import CinemaSeatSelection from './pages/user/CinemaSeatSelection';
-
-
+import CheckoutPage from './pages/user/CheckoutPage';
+import SuccessfulPayment from './pages/user/SuccessfulPayment';
 import "./i18n";
 
 
@@ -41,8 +41,13 @@ function App() {
         },{
           path: "/movie/:movieId/showtime/:showId/seats",
           element: <CinemaSeatSelection />
+        },{
+          path: "/checkout",
+          element: <CheckoutPage />
+        },{
+          path:"/success",
+          element:<SuccessfulPayment/>
         }
-
 
 
       ]
